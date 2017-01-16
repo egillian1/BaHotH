a44c349e018214ace48efe5d9a00db98e61fe9e2
egill <egill.ian1@gmail.com>
2017-01-16 00:49:55 +0000

    Modularizes character creation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var characterModule = require('./character.js');
 var app = express();
 
 app.use(bodyParser.json());
@@ -24,7 +25,7 @@ app.get('/', function(req, res) {
 });
 
 // Number used to generate character stats
-var characterNumber = 0;
+var characterNumber;
 
 app.post('/character', function(req, res, next){
   characterNumber = req.body.characterNumber;
@@ -33,173 +34,9 @@ app.post('/character', function(req, res, next){
 });
 
 app.get('/character.json', function(req, res, next){
-  console.log(characterNumber);
-  let character = createCharacterFromNumber(characterNumber);
+  let character = characterModule.createCharacterFromNumber(characterNumber);
   res.json({character: character});
 })
 
-// Creates and returns an object containing character stats by using
-// the passed number
-function createCharacterFromNumber(number){
-console.log("Creating character");
-
-  let character = {
-    fullName: "Missy Dubourde",
-    speedArray: [0,3,4,5,6,6,6,7,7],
-    mightArray: [0,2,3,3,3,4,5,6,7],
-    sanityArray: [0,1,2,3,4,5,5,6,7],
-    knowledgeArray: [0,2,3,4,4,5,6,6,6],
-    speedIndex: 3,
-    mightIndex: 4,
-    sanityIndex: 3,
-    knowledgeIndex: 4
-  };
-
-  if(number == 2){
-    character.fullName = "Zoe Ingstrom";
-    character.speedArray = [0,4,4,4,4,5,6,8,8];
-    character.mightArray = [0,2,2,3,3,4,4,6,7];
-    character.sanityArray = [0,3,4,5,5,6,6,7,8];
-    character.knowledgeArray = [0,1,2,3,4,4,5,5,5];
-    character.speedIndex = 4;
-    character.mightIndex = 4;
-    character.sanityIndex = 3;
-    character.knowledgeIndex = 3;
-  }
-
-  if(number == 3){
-    character.fullName = "Darrin 'Flash' Williams";
-    character.speedArray = [0,4,4,4,5,6,7,7,8];
-    character.mightArray = [0,2,3,3,4,5,6,6,7];
-    character.sanityArray = [0,1,2,3,4,5,5,5,7];
-    character.knowledgeArray = [0,2,3,4,5,5,5,7];
-    character.speedIndex = 5;
-    character.mightIndex = 3;
-    character.sanityIndex = 3;
-    character.knowledgeIndex = 3;
-  }
-
-  if(number == 4){
-    character.fullName = "Ox Bellows";
-    character.speedArray = [0,2,2,2,3,4,5,5,6];
-    character.mightArray = [0,4,5,5,6,6,7,8,8];
-    character.sanityArray = [0,2,2,3,4,5,5,6,7];
-    character.knowledgeArray = [0,2,2,3,3,5,5,6,6];
-    character.speedIndex = 5;
-    character.mightIndex = 3;
-    character.sanityIndex = 3;
-    character.knowledgeIndex = 3;
-  }
-
-  if(number == 5){
-    character.fullName = "Brandon Jaspers";
-    character.speedArray = [0,3,4,4,4,5,6,7];
-    character.mightArray = [0,2,3,3,4,5,6,6,7];
-    character.sanityArray = [0,3,3,3,4,5,6,7,8];
-    character.knowledgeArray = [0,1,3,3,5,5,6,6,7];
-    character.speedIndex = 3;
-    character.mightIndex = 4;
-    character.sanityIndex = 4;
-    character.knowledgeIndex = 3;
-  }
-
-  if(number == 6){
-    character.fullName = "Peter Akimoto";
-    character.speedArray = [0,3,3,3,4,6,6,7,7];
-    character.mightArray = [0,2,3,3,4,5,5,6,8];
-    character.sanityArray = [0,3,4,4,4,5,6,6,7];
-    character.knowledgeArray = [0,3,4,4,5,6,7,7,8];
-    character.speedIndex = 4;
-    character.mightIndex = 3;
-    character.sanityIndex = 4;
-    character.knowledgeIndex = 3;
-  }
-
-  if(number == 7){
-    character.fullName = "Madame Zostra";
-    character.speedArray = [0,2,3,3,5,5,6,6,7];
-    character.mightArray = [0,2,3,3,4,5,5,5,6];
-    character.sanityArray = [0,4,4,4,5,6,7,8,8];
-    character.knowledgeArray = [0,1,3,4,4,4,5,6,6];
-    character.speedIndex = 3;
-    character.mightIndex = 4;
-    character.sanityIndex = 3;
-    character.knowledgeIndex = 4;
-  }
-
-  if(number == 8){
-    character.fullName = "Vivian Lopez";
-    character.speedArray = [0,3,4,4,4,4,6,7,8];
-    character.mightArray = [0,2,2,2,4,4,5,6,6];
-    character.sanityArray = [0,4,4,4,5,6,7,8,8];
-    character.knowledgeArray = [0,4,5,5,5,5,6,6,7];
-    character.speedIndex = 4;
-    character.mightIndex = 3;
-    character.sanityIndex = 3;
-    character.knowledgeIndex = 4;
-  }
-
-  if(number == 9){
-    character.fullName = "Father Rhinehardt";
-    character.speedArray = [0,2,3,3,4,5,6,7,7];
-    character.mightArray = [0,1,2,2,4,4,5,5,7];
-    character.sanityArray = [0,3,4,5,5,6,7,7,8];
-    character.knowledgeArray = [0,1,3,3,4,5,6,6,8];
-    character.speedIndex = 3;
-    character.mightIndex = 3;
-    character.sanityIndex = 5;
-    character.knowledgeIndex = 4;
-  }
-
-  if(number == 9){
-    character.fullName = "Father Rhinehardt";
-    character.speedArray = [0,2,3,3,4,5,6,7,7];
-    character.mightArray = [0,1,2,2,4,4,5,5,7];
-    character.sanityArray = [0,3,4,5,5,6,7,7,8];
-    character.knowledgeArray = [0,1,3,3,4,5,6,6,8];
-    character.speedIndex = 3;
-    character.mightIndex = 3;
-    character.sanityIndex = 5;
-    character.knowledgeIndex = 4;
-  }
-
-  if(number == 10){
-    character.fullName = "Professor Longfellow";
-    character.speedArray = [0,2,2,4,4,5,5,6,6];
-    character.mightArray = [0,1,2,3,4,5,5,6,6];
-    character.sanityArray = [0,1,3,3,4,5,5,6,7];
-    character.knowledgeArray = [0,4,5,5,5,5,6,7,8];
-    character.speedIndex = 4;
-    character.mightIndex = 3;
-    character.sanityIndex = 3;
-    character.knowledgeIndex = 5;
-  }
-
-  if(number == 11){
-    character.fullName = "Jenny LeClerc";
-    character.speedArray = [0,2,3,4,4,4,5,6,8];
-    character.mightArray = [0,3,4,4,4,4,5,6,8];
-    character.sanityArray = [0,1,1,2,4,4,4,5,6];
-    character.knowledgeArray = [0,2,3,3,4,4,5,6,8];
-    character.speedIndex = 4;
-    character.mightIndex = 3;
-    character.sanityIndex = 5;
-    character.knowledgeIndex = 3;
-  }
-
-  if(number == 12){
-    character.fullName = "Heather Granville";
-    character.speedArray = [0,3,3,4,5,6,6,7,8];
-    character.mightArray = [0,3,3,3,4,5,6,7,8];
-    character.sanityArray = [0,3,3,3,4,5,6,6,6];
-    character.knowledgeArray = [0,2,3,3,4,5,6,7,8];
-    character.speedIndex = 3;
-    character.mightIndex = 3;
-    character.sanityIndex = 3;
-    character.knowledgeIndex = 5;
-  }
-
-  return character;
-}
 
 app.listen(5000);
